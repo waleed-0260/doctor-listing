6f5628fa1d6a83266374adc9ef6e8b5f7577d14e
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2025-02-23 15:59:27 -0800

    csfsdf

--- utils/getDoctors.ts
+++ utils/getDoctors.ts
@@ -1,6 +1,6 @@
 export const getDoctors = async () => {
     try {
-      const res = await fetch(`/api/users/mongoose`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/mongoose`, {
         cache: "no-store", // Ensure fresh data on every request
       });
   
